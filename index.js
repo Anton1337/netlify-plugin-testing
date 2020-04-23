60d9e85dbcbede4b5c3274ae7974028e5b39af59
Anton <anton_real_madrid@icloud.com>
2020-04-23 21:04:50 +0200

    added plugin lifetime methods

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 module.exports = {
+  onInit: ({ inputs }) => {
+    console.log('onInit Running...');
+  },
   onPreBuild: ({ inputs }) => {
-    console.log('onPreBuild Runs');
+    console.log('onPreBuild Running...');
+  },
+  onBuild: ({ inputs }) => {
+    console.log('onBuild Running...');
+  },
+  onPostBuild: ({ inputs }) => {
+    console.log('onPostBuild Running...');
+  },
+  onSuccess: ({ inputs }) => {
+    console.log('onSuccess Running...');
+  },
+  onError: ({ inputs }) => {
+    console.log('onError Running...');
+  },
+  onEnd: ({ inputs }) => {
+    console.log('onEnd Running...');
   },
 };
